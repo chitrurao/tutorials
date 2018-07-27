3897155d2458120dbde6c045ce844e22a147bbc8
Chitru <chitrurao@gmail.com>
2018-07-27 17:12:45 +0530

    fixed bug in ball

--- pong/public/pong.js
+++ pong/public/pong.js
@@ -46,14 +46,14 @@ class Pong{
         this._context = canvas.getContext('2d');
  
         this.ball=new Ball;
-        this.ball.pos.x=100
-        this.ball.pos.y=50
-        this.ball.vel.x = 100
-        this.ball.vel.y = 100
+        this.ball.pos.x=100;
+        this.ball.pos.y=50;
+        this.ball.vel.x = 100;
+        this.ball.vel.y = 100;
         let lastTime;
-        const callback(millis)=>{
+        const callback = (millis) => {
             if(lastTime){
-                update((millis - lastTime)/1000)
+                this.update((millis - lastTime)/1000)
             }
             lastTime = millis;
             window.requestAnimationFrame(callback);
@@ -62,13 +62,13 @@ class Pong{
     }
 
     update(dt){
-        this.ball.pos.x += ball.vel.x*dt;
-        this.ball.pos.y += ball.vel.y*dt;
+        this.ball.pos.x += this.ball.vel.x*dt;
+        this.ball.pos.y += this.ball.vel.y*dt;
     
          //check if the ball is hitting the wall, and if so , 
          //reverse the direction
     
-         if ( this.ball.left < 0 || ball.right > this._canvas.width ){
+         if ( this.ball.left < 0 || this.ball.right > this._canvas.width ){
              this.ball.vel.x = -this.ball.vel.x;
          }
          if ( this.ball.top < 0 || this.ball.bottom > this._canvas.height ){
@@ -78,7 +78,7 @@ class Pong{
         this._context.fillStyle="#0FF";
         this._context.fillRect(0,0, this._canvas.width, this._canvas.height);
         this._context.fillStyle="#F00";
-        this._context.fillRect(this.ball.pos.x,ball.pos.y, 
+        this._context.fillRect(this.ball.pos.x,this.ball.pos.y, 
                                this.ball.size.x,this.ball.size.y);
     }
 }
