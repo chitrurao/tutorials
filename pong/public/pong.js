3551921895755a166766be45fc55fe501cad414d
Chitru <chitrurao@gmail.com>
2018-07-28 15:58:32 +0530

    Added color-changer to the ball

--- pong/public/pong.js
+++ pong/public/pong.js
@@ -29,6 +29,28 @@ class Rect{
     {
         return this.pos.y + this.size.y /2;
     }
+
+    getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 3; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+      }
+
+    changeColor(){
+      //Create a random color
+     this.setColor(this.getRandomColor());
+    }
+
+    setColor(color){
+        this.color = color;
+    }
+
+    getColor(){
+        return this.color;
+    }
 }
 
 class Ball extends Rect {
@@ -39,6 +61,13 @@ class Ball extends Rect {
     }
 }
 
+class Dude extends Rect {
+
+    constructor(startx, starty){
+        super(startx,starty)
+        this.vel = new Vec
+    }
+}
 
 class Pong{
     constructor(canvas){
@@ -50,7 +79,24 @@ class Pong{
         this.ball.pos.y=50;
         this.ball.vel.x = 100;
         this.ball.vel.y = 100;
+        this.ball.setColor("#F00");
         let lastTime;
+
+        this.p1 = new Dude(10,50);
+        this.p1.pos.x=100;
+        this.p1.pos.y=50;
+        this.p1.vel.x = 0;
+        this.p1.vel.y = 0;
+        this.p1.setColor("#FF0");
+
+        
+        this.p2 = new Dude(10,50);
+        this.p2.pos.x=500;
+        this.p2.pos.y=50;
+        this.p2.vel.x = 0;
+        this.p2.vel.y = 0;
+        this.p2.setColor("#0F0");
+
         const callback = (millis) => {
             if(lastTime){
                 this.update((millis - lastTime)/1000)
@@ -59,6 +105,21 @@ class Pong{
             window.requestAnimationFrame(callback);
         }
         callback()
+    }
+
+    drawRect(rect){
+        this._context.fillStyle = rect.getColor();
+        this._context.fillRect(rect.pos.x,rect.pos.y, 
+                               rect.size.x,rect.size.y);   
+    }
+    
+    drawBoard(){
+        this._context.fillStyle="#0FF";
+        this._context.fillRect(0,0, this._canvas.width, this._canvas.height);
+        this.drawRect(this.ball);
+        this.drawRect(this.p1);
+        this.drawRect(this.p2);
+        
     }
 
     update(dt){
@@ -72,18 +133,38 @@ class Pong{
              this.ball.vel.x = -this.ball.vel.x;
          }
          if ( this.ball.top < 0 || this.ball.bottom > this._canvas.height ){
-            this.ball.vel.y = -this.ball.vel.y;
+            this.ball.vel.y = -this.ball.vel.y;   
         }
-    
-        this._context.fillStyle="#0FF";
-        this._context.fillRect(0,0, this._canvas.width, this._canvas.height);
-        this._context.fillStyle="#F00";
-        this._context.fillRect(this.ball.pos.x,this.ball.pos.y, 
-                               this.ball.size.x,this.ball.size.y);
+        this.ball.changeColor();
+        this.drawBoard();
     }
 }
 
 const canvas = document.getElementById("pong");
 const pong = new Pong(canvas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
